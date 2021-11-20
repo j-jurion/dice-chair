af8e3c9887ceb46d70929ce50e56d20e7842a101
Joeri Jurion <jurion@septentrio.com>
2021-11-20 12:55:58 +0100

    Enhanced user experience.

--- dice_chair/dice_chair.ino
+++ dice_chair/dice_chair.ino
@@ -3,8 +3,8 @@ int die_pins [] = {3, 4, 5, 8, 11, 12};
 int button_pin = 2;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Setting up the die...");
+  //Serial.begin(9600);
+  //Serial.println("Setting up the die...");
   for (int i = 0; i < 6; i++) {
     pinMode(die_pins[i], OUTPUT);
   }
@@ -18,8 +18,8 @@ void setup() {
 
 void loop() {
   switchState = digitalRead(button_pin);
-  Serial.print("Switch state: ");
-  Serial.println(switchState);
+  //Serial.print("Switch state: ");
+  //Serial.println(switchState);
   if (switchState == HIGH) {
     roll();
   }
@@ -39,8 +39,8 @@ void startup() {
   }
 }
 void roll() {
-  Serial.println("Rolling...");
-  for (int j = 0; j < 10; j++) {
+  //Serial.println("Rolling...");
+  for (int j = 0; j < 4; j++) {
     for (int i = 0; i < 6; i++) {
       digitalWrite(die_pins[i], HIGH);
       delay(100);
@@ -48,15 +48,11 @@ void roll() {
     }
   }
   showresult();
-  for (int i = 0; i < 6; i++) {
-    digitalWrite(die_pins[i], LOW);
-  }
 }
 void showresult() {
   int r = random(6);
-  Serial.print("Die result: ");
-  Serial.println(r);
+  //Serial.print("Die result: ");
+  //Serial.println(r);
 
   digitalWrite(die_pins[r], HIGH);
-  delay(10000);
 }
